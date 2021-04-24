a0b56ced360014f68c56526642ffe6c88fd8526d
vdpolg <vdpolg@gmail.com>
2021-04-24 18:51:45 +0800

    first release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # math6
 math game for fun
+
+## 緣起
+[math-game-linux-commands](https://opensource.com/article/21/4/math-game-linux-commands)
+
+## 簡述
+定義
+
+1. 產生`小四`數: 從數字1~10取出4個隨機數字( `small 4`)
+2. 產生`大二`數: 從數字15,20,25~100 取出2個隨機數字 ( `large 2`)
+3. 產生`目標`數: 從數字200~999 取出1個隨機數字 ( `target`)
+
+規則
+
+1. 從上述的`小四`和`大二`共`6`個數字的加減乘除運算，要拼湊出 `目標`
+2. 這6個數字最多只可使用1次。
+
+## 版本
+* 0.1 初版，加入 ? 和 -v 查功能
+
+## 待辦
+
+* 讓手機APP `Termux`也能使用 ，若在cmd 下指令是ok的，但寫bash script產出的$RANDOM卻是空值
+
+* 用`true random` 真正的RANDOM
+
+* 產生題目後，可以再產出解法，按鈕提示。(做出來會很爽)
---
+++ math6.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 名稱       版本    日期            作者            備註
+# math6      v0.1    20210424        arthur_lai      初版
+# 參考來源 https://opensource.com/article/21/4/math-game-linux-commands
+if [[ "${1}" == "?" ]] || [[ "${1}" == "-v" ]] ; then
+	echo "Version : 0.1"
+	exit 
+else	
+	echo -n 'small 4 : '
+	echo `seq 1 10 | shuf | head -4 `
+	echo -n 'large 2 : '
+	echo `seq 15 5 100 | shuf | head -2`
+	echo -n 'target  : '
+	echo `shuf -i 200-999 -n 1`
+fi
+	echo ======Test for Termux =========================
+	echo -n 'Use RANDOM : '
+	echo $RANDOM
+
