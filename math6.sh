8ae5bbe9c8862dd5421cab2bc931ba89b39b834b
vdpolg <vdpolg@gmail.com>
2021-04-24 22:58:17 +0800

    檢核`$1`參數只能輸入1組

--- math6.sh
+++ math6.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # 名稱       版本    日期            作者            備註
 # math6      v0.1    20210424        arthur_lai      初版
+# math6      v0.1.1  20210424        arthur_lai      二版
 # 參考來源 https://opensource.com/article/21/4/math-game-linux-commands
-if [[ "${1}" == "?" ]] || [[ "${1}" == "-v" ]] ; then
-	echo "Version : 0.1"
+Version=0.1.1
+if 
+	[[ $# -gt 1 ]] ; then
+	echo '你只能輸入一個參數'
 	exit 
-else	
+elif
+	[[ "${1}" == "?" ]] || [[ "${1}" == "-v" ]] || [[ "${1}" == "-V" ]] || [[ "${1}" == "-?" ]]; then
+	echo "Version : $Version"
+	exit
+else
 	echo -n 'small 4 : '
 	echo `seq 1 10 | shuf | head -4 `
 	echo -n 'large 2 : '
